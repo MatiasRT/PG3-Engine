3ea750c0470971828c966137f9c6c0a5b2cead8f
MatiasRT <ruiztorresmatias@gmail.com>
2019-04-05 22:12:16 -0300

    Commit #4
    - Comment of how rotations should be called (WIP)

--- Engine/TestEngine/Game.cpp
+++ Engine/TestEngine/Game.cpp
@@ -91,8 +91,10 @@ bool Game::OnUpdate() {																			// Toda la logica va aca
 	/* CAMARA */
 	//renderer->TranslateCamera(glm::vec3(speed * time, 0, 0));
 	//camera->Walk(speed * time, -0.1f * time);								// mov
-	//camera->Yaw(0.2f * time);												// rot 
-	camera->Pitch(0.2f * time, 0);											// otra rot
+	//camera->Yaw(0.2f * time);												// este es roll
+	//camera->Pitch(0, 0.2f * time);										// este es pitch
+	camera->Pitch(0.2f * time, 0);											// esto es yaw
+
 		
 	/* ANIMACIONES*/
 	sp1->UpdateAnim(time);
