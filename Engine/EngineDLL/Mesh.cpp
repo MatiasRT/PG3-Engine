d7d154cc21c8e6f28b6648feeba76f226e952d80
Matias <ruiztorresmatias@gmail.com>
2019-07-15 16:24:46 -0300

    Commit #33
    - Mesh commented

--- Engine/EngineDLL/Mesh.cpp
+++ Engine/EngineDLL/Mesh.cpp
@@ -7,7 +7,7 @@ Mesh::Mesh(Renderer * renderer, const char * txtFile, Camera * cam) : Component(
 
 	textureFile = txtFile;
 	meshInfo = new MeshInfo;
-	meshData = new MeshData();
+	meshData = new MeshData;
 
 	meshInfo->shouldDisposeVertices = false;
 	meshInfo->shouldDisposeIndices = false;
@@ -30,42 +30,42 @@ Mesh::~Mesh() {
 
 void Mesh::SetMeshData(MeshData * meshD) {
 	meshData = meshD;
-	meshInfo->vtxCount = meshData->vertices->size();
-	meshInfo->idxCount = meshData->indices->size();
-	meshInfo->textureCount = meshData->textures->size();
+	meshInfo->vtxCount = meshData->vertices->size();														// Guardo la cantidad de vertices que tiene el meshData luego de ser cargado en InitMesh de importer
+	meshInfo->idxCount = meshData->indices->size();															// Guardo la cantidad de indices que tiene el meshData luego de ser cargado en InitMesh de importer
+	meshInfo->textureCount = meshData->textures->size();													// Guardo la cantidad de texuras que tiene el meshData luego de ser cargado en InitMesh de importer
 
 	meshInfo->vertex = new float[meshInfo->vtxCount];
 	for (int i = 0; i < meshInfo->vtxCount; i++)
-		meshInfo->vertex[i] = meshData->vertices->at(i);
+		meshInfo->vertex[i] = meshData->vertices->at(i);													// Cargo el vector de vertices de meshInfo con los vertices de meshData
 
 	DisposeVertices();
 
 	meshInfo->shouldDisposeVertices = true;
-	meshInfo->bufferId = renderer->GenBuffer(meshInfo->vertex, sizeof(float)* meshInfo->vtxCount);
+	meshInfo->bufferId = renderer->GenBuffer(meshInfo->vertex, sizeof(float)* meshInfo->vtxCount);			// Genero el buffer para los vertices
 
 
 	meshInfo->textureVertex = new float[meshInfo->textureCount];
 	for (int k = 0; k < meshInfo->textureCount; k++) 
-		meshInfo->textureVertex[k] = meshData->textures->at(k);
+		meshInfo->textureVertex[k] = meshData->textures->at(k);												// Cargo el vector de texturas de meshInfo con las texturas de meshData
 
 	DisposeTexture();
 
 	meshInfo->shouldDisposeTexture = true;
-	meshInfo->textureBufferId = renderer->GenBuffer(meshInfo->textureVertex, sizeof(float)* meshInfo->textureCount);
+	meshInfo->textureBufferId = renderer->GenBuffer(meshInfo->textureVertex, sizeof(float)* meshInfo->textureCount); // Genero el buffer para las texturas
 
 	meshInfo->indices = new unsigned int[meshInfo->idxCount];
 	for (int j = 0; j < meshInfo->idxCount; j++) 
-		meshInfo->indices[j] = meshData->indices->at(j);
+		meshInfo->indices[j] = meshData->indices->at(j);													// Cargo el vector de indices de meshInfo con los indices de meshData
 	
 	DisposeIndices();
 
 	meshInfo->shouldDisposeIndices = true;
-	meshInfo->indexBufferId = renderer->GenElementBuffer(meshInfo->indices, sizeof(int)* meshInfo->idxCount);
+	meshInfo->indexBufferId = renderer->GenElementBuffer(meshInfo->indices, sizeof(int)* meshInfo->idxCount);		// Genero el buffer para los indices
 
-	meshInfo->material = new Material();
-	meshInfo->programId = meshInfo->material->LoadShaders("VertexTexture.glsl", "FragmentTexture.glsl");
+	meshInfo->material = new Material();																	// Creo el material
+	meshInfo->programId = meshInfo->material->LoadShaders("VertexTexture.glsl", "FragmentTexture.glsl");	// Le cargo los shaders
 
-	header = Importer::LoadBMP(textureFile);
+	header = Importer::LoadBMP(textureFile);																// Cargo la textura en formato bmp
 	meshInfo->textureId = renderer->UploadData(header.width, header.height, header.data);
 	meshInfo->material->BindTexture();
 }
@@ -78,12 +78,12 @@ void Mesh::Draw() {
 			meshInfo->material->SetMatrixProperty(renderer->GetWVP());
 		}
 		renderer->BindTexture(meshInfo->textureId, meshInfo->textureBufferId);
-		renderer->BeginDraw(0);																									// Le decimos al renderer que comience a dibujar
+		renderer->BeginDraw(0);																				// Le decimos al renderer que comience a dibujar
 		renderer->BeginDraw(1);
-		renderer->BindBuffer(meshInfo->bufferId, 0);																			// Unimos el buffer con el buffer binding point
+		renderer->BindBuffer(meshInfo->bufferId, 0);														// Unimos el buffer con el buffer binding point
 		renderer->BindBuffer(1, meshInfo->textureBufferId, 2);
-		renderer->DrawElementBuffer(meshInfo->idxCount, meshInfo->indexBufferId);												// El renderer dibuja el triangulo
-		renderer->EndDraw(0);																									// Deja de dibujar
+		renderer->DrawElementBuffer(meshInfo->idxCount, meshInfo->indexBufferId);							// El renderer dibuja el triangulo
+		renderer->EndDraw(0);																				// Deja de dibujar
 		renderer->EndDraw(1);
 	}
 	else
